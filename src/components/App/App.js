1d0e5094001bb73a9d0e0ecb9b3918a7875b1925
AntonSidaras <sian111@yandex.ru>
2021-11-06 23:46:14 +0300

    basics_App+Main_06.11.2021_23:46

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import NotFound from '../NotFound/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <CurrentUserContext.Provider value={{}}>
+        <div className="app app__content">
+          <Routes>
+            {/* О проекте */}
+            <Route exact path="/" element={<Main />} />
+            {/* Фильмы */}
+            <Route exact path="/movies" element={<Movies />} />
+            {/* Сохранённые фильмы */}
+            <Route exact path="/saved-movies" element={<SavedMovies />} />
+            {/* Профиль */}
+            <Route exact path="/profile" element={<Profile />} />
+            {/* Вход */}
+            <Route exact path="/signin" element={<Login />} />
+            {/* Регистрация */}
+            <Route exact path="/signup" element={<Register />} />
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </CurrentUserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import John from '../../images/404/john.gif';
+import './NotFound.css'
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <img className="not-found__picture" src={John} alt="" />
+      <h3 className="not-found__caption">
+        Страница не найдена. Ошибка
+        <span> 404</span>
+      </h3>
+      <Link className="not-found__back" to="/">Назад</Link>
+    </div>
+  )
+}
+
+export default NotFound;
